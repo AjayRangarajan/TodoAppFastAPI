6b4091a35790c1849c3e28a0097c6064ce4dc2ca
AjayRangarajan <ajayrangarajan5@gmail.com>
2022-09-29 10:13:50 +0530

    Add requirements.txt file and add .env file for DATABASE_URI

--- database.py
+++ database.py
@@ -1,8 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-engine = create_engine("sqlite:///todo.db")
+load_dotenv()
+
+
+DATABASE_URI = os.getenv("DATABASE_URI")
+print(DATABASE_URI)
+
+engine = create_engine(DATABASE_URI)
 
 Base = declarative_base()
 
